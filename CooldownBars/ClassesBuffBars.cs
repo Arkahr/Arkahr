fba2a36a10894722daa93a46f1a9337f6f20a33b
Arkahr <41120841+Arkahr@users.noreply.github.com>
2018-07-12 17:27:30 +0200

    fix

--- CooldownBars/ClassesBuffBars.cs
+++ CooldownBars/ClassesBuffBars.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace Turbo.Plugins.Arkahr
 {
-    //link test
+
     public class ClassesBuffBars : BasePlugin, IInGameTopPainter
     {        
         public CooldownBarsPainter CooldownBarsPainter {get;set;}
